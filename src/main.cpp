f9fdec8a33d2d0884fe246db1bfdec28edf0735d
mehravarun666 <mehra.varun666@gmail.com>
2026-07-15 22:53:50 +0530

    Build project and run Drogon server

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
+#include <drogon/drogon.h>
 
 int main()
 {
-    std::cout << "url-shortener starting...\n";
+    drogon::app().registerHandler(
+        "/",
+        [](const drogon::HttpRequestPtr&,
+           std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+            auto resp = drogon::HttpResponse::newHttpResponse();
+            resp->setBody(R"({"status":"ok","service":"url-shortener"})");
+            resp->setContentTypeCode(drogon::CT_APPLICATION_JSON);
+            callback(resp);
+        });
+
+    drogon::app().addListener("0.0.0.0", 8080);
+    drogon::app().run();
     return 0;
 }
